608bbaab1f30c46aeacb0bbaa7e9838423551418
casti-lob <casti_lob@hotmail.com>
2023-01-23 18:41:54 +0100

    ejercicio basico terminado

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import {createRoot}  from "react-dom/client";
+import CounterApp from "./CounterApp";
 //import { ReactDOM } from "react-dom";
 import './index.css';
-import PrimeraApp from "./PrimeraApp";
+//import PrimeraApp from "./PrimeraApp";
+
 
 const container = document.querySelector('#root');
-const saludo=<h1>Hola mundo</h1>;
+//const saludo=<h1>Hola mundo</h1>;
 
 const root=createRoot(container);
-root.render(<PrimeraApp nombre={"María"}  />)
+root.render(<CounterApp value={20}  />)
 //ReactDOM.render(saludo, root);
 
 
